aa44ec459f8e1baab04b739f40197672bc5f1b71
shilpa <ashilpa.163@gmail.com>
2020-08-05 19:39:58 +0530

    commit

---
+++ doc13.py
@@ -0,0 +1,9 @@
+a = int(input("enter 1st number : "))
+b = int(input("enter 2nd number: "))
+c = int(input("enter 3rd number : "))
+if a > b and a > c:
+    print("% is greater :" %(a))
+elif a > b and a > c:
+    print("% is greater :" % (b))
+else:
+    print("%d is greater : "%(c))
